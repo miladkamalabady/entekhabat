df4dcd478331b37024999b3b77368fa320b94eef
miladkamalabady <miladkamalabady@gmail.com>
2026-06-28 21:12:19 +0330

    ta

--- api-dotnet/obj/Debug/net8.0/win-x64/EntekhabatApi.AssemblyInfo.cs
+++ api-dotnet/obj/Debug/net8.0/win-x64/EntekhabatApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EntekhabatApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b558e41e56e49d60b42719652243d6e27f4a3c29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0b512986be7a07b323fd09facb27526d62c9350")]
 [assembly: System.Reflection.AssemblyProductAttribute("EntekhabatApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EntekhabatApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
